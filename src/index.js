5de41cc86164937c512173205c58a0b42e8cc5d4
Filipp <wolverinexdd@yandex.ru>
2025-04-24 01:46:06 +0400

    added course section and slider swiper

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import './styles/main.scss'
+import '../src/styles/normalize.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './index.html'
+import './styles/main.scss'
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.slider-teacher', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    modules: [Navigation, Pagination],
+  });
 
